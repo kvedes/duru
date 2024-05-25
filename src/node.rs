22d6e75e55df937bb139646912cb659ff91571e4
matkvist <bejsebakkevej@hotmail.com>
2024-05-25 16:00:37 +0200

    add file list

--- src/node.rs
+++ src/node.rs
@@ -6,8 +6,12 @@ use std::path::{Path, PathBuf};
 #[derive(Debug)]
 enum DuruError {
     NotADir,
+    NotAFile,
     IsLeaf,
+    NotRoot,
     ExistingChildren,
+    NoChildren,
+    RootCantBeChild,
 }
 
 #[derive(Clone, Debug)]
@@ -97,4 +101,115 @@ impl Node {
             })
             .collect()
     }
+
+    pub fn file_list(&self) -> Result<&mut Vec<DuruFile>, DuruError> {
+        match self {
+            Node::Root { children, path } => {
+                if children.is_some() {
+                    self.file_list_recurse(&mut Vec::new())
+                } else {
+                    Err(DuruError::NoChildren)
+                }
+            }
+            Node::Dir {
+                name,
+                path,
+                size,
+                children,
+            } => Err(DuruError::NotRoot),
+            Node::File { name, path, size } => Err(DuruError::IsLeaf),
+        }
+    }
+
+    fn file_list_recurse(
+        &self,
+        file_list: &mut Vec<DuruFile>,
+    ) -> Result<&mut Vec<DuruFile>, DuruError> {
+        match self {
+            Node::Root { children, path } => {
+                if let Some(children) = children.as_mut() {
+                    for child in children.iter_mut() {
+                        if let Node::Root { .. } = child {
+                            return Err(DuruError::RootCantBeChild);
+                        }
+                        if let Node::Dir { .. } = child {
+                            self.file_list_recurse(file_list)?;
+                        }
+                        if let Node::File { name, path, size } = child {
+                            file_list.push(DuruFile::new(
+                                name.to_string(),
+                                path.to_str().unwrap().to_string(),
+                                *size,
+                            ));
+                        }
+                    }
+                }
+                Ok(file_list)
+            }
+            Node::Dir {
+                name,
+                path,
+                size,
+                children,
+            } => {
+                if let Some(children) = children.as_mut() {
+                    for child in children.iter_mut() {
+                        if let Node::Root { .. } = child {
+                            return Err(DuruError::RootCantBeChild);
+                        }
+                        if let Node::Dir { .. } = child {
+                            self.file_list_recurse(file_list)?;
+                        }
+                        if let Node::File { name, path, size } = child {
+                            file_list.push(DuruFile::new(
+                                name.to_string(),
+                                path.to_str().unwrap().to_string(),
+                                *size,
+                            ));
+                        }
+                    }
+                }
+                Ok(file_list)
+            }
+            Node::File { name, path, size } => {
+                file_list.push(DuruFile::new(
+                    name.to_string(),
+                    path.to_str().unwrap().to_string(),
+                    *size,
+                ));
+                Ok(file_list)
+            }
+        }
+    }
 }
+
+struct DuruFile {
+    name: String,
+    path: String,
+    size: u64,
+}
+
+impl DuruFile {
+    pub fn new(name: String, path: String, size: u64) -> Self {
+        DuruFile { name, path, size }
+    }
+
+    pub fn from_node(node: Node) -> Result<Self, DuruError> {
+        match node {
+            Node::File { name, path, size } => Ok(DuruFile::new(
+                name,
+                path.to_str().unwrap().to_string(),
+                size,
+            )),
+            Node::Root { children, path } => Err(DuruError::NotAFile),
+            Node::Dir {
+                name,
+                path,
+                size,
+                children,
+            } => Err(DuruError::NotAFile),
+        }
+    }
+}
+
+//pub fn files_to_list(root: Node) -> Vec<DuruFile> {}
